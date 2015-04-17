eea918f6096ee790177b5378138f67f68fc2c4ef
Serge Blagodatskih <blagodatskih@insoft.ru>
2015-04-17 16:57:38 +0300

    карусель почти доделал

--- insoft/urls.py
+++ insoft/urls.py
@@ -27,3 +27,4 @@ if settings.DEBUG:
 
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL + 'images/', document_root=os.path.join(settings.MEDIA_ROOT, 'images'))
+    urlpatterns += static(settings.MEDIA_URL + 'original_images/', document_root=os.path.join(settings.MEDIA_ROOT, 'original_images'))
